b87da414eb49144baa306f820b6ffcbf1fa26302
Austin Wise <AustinWise@gmail.com>
2016-02-05 19:44:11 -0800

    Add austin to the wheel group.

--- addUsers.sh
+++ addUsers.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+set -e
+set -x
+
 groupadd -g 1001 media
 groupadd -g 1002 gmedia
 groupadd -g 1003 austin
 groupadd -g 1004 g
+groupadd wheel || true
 
-useradd -s /usr/bin/bash -g austin -G media -m -u 1001 austin
+useradd -s /usr/bin/bash -g austin -G media,wheel -m -u 1001 austin
 useradd -s /usr/bin/bash -g g -G gmedia -m -u 1002 g
 
