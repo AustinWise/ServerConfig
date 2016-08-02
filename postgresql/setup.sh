a59d6a4c4b98e8d04755db52889a73f9c8f29cac
Austin Wise <AustinWise@gmail.com>
2016-08-02 00:51:05 -0700

    Add postgresql setup commands.

---
+++ postgresql/setup.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+set -e
+
+svcadm disable postgresql
+mv /var/pgsql /var/pgsql_old
+zfs create -o recordsize=8192 -o compression=lz4 -o mountpoint=/var/pgsql $(zfs list -H -o name /)/data/pgsql
+cp -R /var/pgsql_old/data /var/pgsql/
+chown -R postgres:postgres /var/pgsql
