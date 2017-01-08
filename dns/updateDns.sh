132459288707ef430b260448e61c351286503a5c
Austin Wise <AustinWise@gmail.com>
2017-01-08 02:57:52 -0800

    A script for updating dynamic DNS from cron.

---
+++ dns/updateDns.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#set -e
+#set -x
+PASSWORD=$(cat dnsPassword.txt)
+curl "https://dynamicdns.park-your-domain.com/update?by=nc&host=kyouei&domain=awise.us&password=$PASSWORD"
+# TODO: check result
