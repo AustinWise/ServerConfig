5683e4899c9080be70bc5f6e50bbae5d6ebaa8f5
Austin Wise <AustinWise@gmail.com>
2017-02-06 00:55:07 -0800

    Update backup script.

--- sendBackup.sh
+++ sendBackup.sh
@@ -3,16 +3,16 @@ set -e
 
 #send initally with -p so that properties get sent
 
-remoteIp=10.5.2.4
+remoteIp=192.168.1.8
 poolName=zones
-thingsToSync="b259c996-dce5-40e7-b787-f69bf94f6f49-disk0 home/austin home/g shares/crap shares/backup shares/gdata shares/isos shares/music shares/photos shares/software shares/videos"
+thingsToSync="c1b61aba-756c-e3a5-fa73-83333b899618/data/pgsql b259c996-dce5-40e7-b787-f69bf94f6f49-disk0 home/austin home/g shares/crap shares/backup shares/gdata shares/isos shares/music shares/photos shares/software shares/videos"
 
 for fs in $thingsToSync; do
   remoteVersion=$(ssh root@$remoteIp zfs list -H -o name -t snapshot -r $poolName/$fs | grep monthly | tail -1l | sed -e "s/^.*@//")
   localVersion=$(zfs list -H -o name -t snapshot -r $poolName/$fs | grep monthly | tail -1l | sed -e "s/^.*@//")
   if [ "$remoteVersion" != "$localVersion" ]; then
     echo $fs needs sync, local: $localVersion remote: $remoteVersion
-    zfs send -e -v -I $remoteVersion $poolName/$fs@$localVersion | ssh root@$remoteIp zfs receive $poolName/$fs
+    zfs send -LcevI $remoteVersion $poolName/$fs@$localVersion | ssh root@$remoteIp zfs receive $poolName/$fs
   else
     echo $fs is up to date
   fi
