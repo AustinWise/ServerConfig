f510ecfb1ffe1427dc987be925cf47e31bb06834
Austin Wise <AustinWise@gmail.com>
2017-04-09 18:07:43 -0700

    Skipp backing up dropbox.
    It is only supposed to be a temporary place.

--- thumb_drive_backup/listBackupDatasets.sh
+++ thumb_drive_backup/listBackupDatasets.sh
@@ -3,5 +3,5 @@ set -e
 set -o pipefail
 
 zfs list -r -H -o name zones/home | sed -e '1d'
-zfs list -r -H -o name zones/shares | sed -e '1d'
+zfs list -r -H -o name zones/shares | sed -e '1d' | grep -v dropbox
 echo zones/c1b61aba-756c-e3a5-fa73-83333b899618/data/pgsql
