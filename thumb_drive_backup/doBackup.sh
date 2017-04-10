393c909fc67eb3375c45b13f08b460dd77fba04d
Austin Wise <AustinWise@gmail.com>
2017-04-09 17:41:24 -0700

    Don't overwrite backups and delete file on backup failure.

--- thumb_drive_backup/doBackup.sh
+++ thumb_drive_backup/doBackup.sh
@@ -19,13 +19,28 @@ do
         ALL_SNAPS=$(zfs list -d 1 -t snap -H -o name $DS | grep @monthly)
         EXISTING_SNAP=this_snap_name_does_not_exist
         LAST_SNAP=$(echo "$ALL_SNAPS" | tail -n 1)
+	TARGET_FILE=./newBackup/$SHORT_NAME
+
+
+	if [ -f $TARGET_FILE ]
+        then
+        echo skipping $DS, target $TARGET_FILE already exists
+		continue
+	fi
+
+
         for SNAP in $ALL_SNAPS
         do
                 if grep $SNAP ./backupSnaps/$SHORT_NAME >/dev/null
                 then
                         EXISTING_SNAP=$SNAP
                 else
-                        zfs send -Lecv -I $EXISTING_SNAP $LAST_SNAP > ./newBackup/$SHORT_NAME
+                        if ! zfs send -Lecv -I $EXISTING_SNAP $LAST_SNAP > $TARGET_FILE
+			then
+				rm $TARGET_FILE
+				echo FAILED: $DS from $EXISTING_SNAP to $LAST_SNAP
+				exit 1
+			fi
                         break;
                 fi
         done
