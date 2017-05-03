94b5711489e335ee3c87b13f5d4a8ae19b5a0b8e
Austin Wise <AustinWise@gmail.com>
2017-05-02 20:57:09 -0700

    Switch to using Cygwin until WSL works better.
    Rsync freezes everytime on the same file. There is already a bug on the
    WSL Github repo for this.

--- windowsBackup.sh
+++ windowsBackup.sh
@@ -5,13 +5,15 @@ set -x
 
 RSYNC_OPT="-rt --delete -e ssh"
 DEST=root@10.5.2.3:/zones/shares/backup/rsync
+#SRC=/mnt
+SRC=/cygdrive
 
-rsync $RSYNC_OPT /mnt/d/src/ $DEST/src/
-rsync $RSYNC_OPT /mnt/d/AustinWise/ $DEST/AustinWise/
-rsync $RSYNC_OPT /mnt/c/Users/AustinWise/Downloads/ $DEST/DownAustin/
-rsync $RSYNC_OPT /mnt/c/Users/AustinWise/Desktop/ $DEST/DesktopAustin/
-rsync $RSYNC_OPT /mnt/d/G/ $DEST/G/
-rsync $RSYNC_OPT /mnt/c/Users/G/Downloads/ $DEST/DownG/
-rsync $RSYNC_OPT /mnt/c/Users/G/Desktop/ $DEST/DesktopG/
+rsync $RSYNC_OPT $SRC/d/src/ $DEST/src/
+rsync $RSYNC_OPT $SRC/d/AustinWise/ $DEST/AustinWise/
+rsync $RSYNC_OPT $SRC/c/Users/AustinWise/Downloads/ $DEST/DownAustin/
+rsync $RSYNC_OPT $SRC/c/Users/AustinWise/Desktop/ $DEST/DesktopAustin/
+rsync $RSYNC_OPT $SRC/d/G/ $DEST/G/
+rsync $RSYNC_OPT $SRC/c/Users/G/Downloads/ $DEST/DownG/
+rsync $RSYNC_OPT $SRC/c/Users/G/Desktop/ $DEST/DesktopG/
 
 echo done
