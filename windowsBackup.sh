43e662882304f067da6be44a9b528c76daddd6bb
Austin Wise <AustinWise@gmail.com>
2017-11-21 07:27:23 +0000

    Add backing up Firefox profiles.

--- windowsBackup.sh
+++ windowsBackup.sh
@@ -12,9 +12,11 @@ rsync $RSYNC_OPT $SRC/d/src/ $DEST/src/
 rsync $RSYNC_OPT $SRC/d/AustinWise/ $DEST/AustinWise/
 rsync $RSYNC_OPT $SRC/c/Users/AustinWise/Downloads/ $DEST/DownAustin/
 rsync $RSYNC_OPT $SRC/c/Users/AustinWise/Desktop/ $DEST/DesktopAustin/
+rsync $RSYNC_OPT $SRC/c/Users/AustinWise/AppData/Roaming/Mozilla/Firefox/Profiles/kqn8ogux.default/ $DEST/FirefoxAustin/
 rsync $RSYNC_OPT $SRC/d/G/ $DEST/G/
 rsync $RSYNC_OPT $SRC/c/Users/G/Downloads/ $DEST/DownG/
 rsync $RSYNC_OPT $SRC/c/Users/G/Desktop/ $DEST/DesktopG/
+rsync $RSYNC_OPT $SRC/c/Users/G/AppData/Roaming/Mozilla/Firefox/Profiles/r82h6mbk.default/ $DEST/FirefoxG/
 
 echo done
 
