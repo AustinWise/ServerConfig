d3dab29f29788479160fa6067a2051f309bf8f82
Austin Wise <AustinWise@gmail.com>
2020-02-02 23:04:56 -0800

    Search for the most recent snapshot for incremental backup. And other fixes.
    * Add an option to use any snapshot, not just monthly.
    * Explicitly sort by txg when searching for parent snapshot.

--- thumb_drive_backup/doBackup.sh
+++ thumb_drive_backup/doBackup.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-set -e
-set -o pipefail
-set -x
+set -euxo pipefail
 
 NO_SNAP_NAME=this_snap_name_does_not_exist
+
+BACKUP_ALL=0
+
+while [[ $# > 0 ]]; do
+        case "$1" in
+                --all)
+                        BACKUP_ALL=1
+                ;;
+                *)
+                        echo Unknown option: $0
+                        exit 1
+                ;;
+        esac
+        shift
+done
 
 if [ $(hostname) != 'kyouei-global' ]; then
         echo not kyouei >&2
@@ -14,13 +27,20 @@ for DS in $(./listBackupDatasets.sh)
 do
         SHORT_NAME=$(echo $DS | sed -e 's/.*\///')
         BACKUP_SNAPS=./backupSnaps/$SHORT_NAME
+        REMOTE_SNAP=$NO_SNAP_NAME
+
         if [ ! -f $BACKUP_SNAPS ]
         then
+                echo No existing snapshot names for ${SHORT_NAME}, skipping
                 continue
         fi
-        ALL_SNAPS=$(zfs list -d 1 -t snap -H -o name $DS | grep @monthly)
-        EXISTING_SNAP=$NO_SNAP_NAME
-        LAST_SNAP=$(echo "$ALL_SNAPS" | tail -n 1)
+
+        LOCAL_SNAPS=$(zfs list -d 1 -t snap -H -o name -s createtxg $DS)
+        if [ $BACKUP_ALL == 0 ]
+        then
+                LOCAL_SNAPS=$(echo "$LOCAL_SNAPS" | grep @monthly)
+        fi
+        SNAP_TO_SEND=$(echo "$LOCAL_SNAPS" | tail -n 1)
         TARGET_FILE=./newBackup/$SHORT_NAME
 
 
@@ -30,30 +50,31 @@ do
                 continue
         fi
 
-        for SNAP in $ALL_SNAPS
+        # find the most recent snapshot to start from
+        for SNAP in $LOCAL_SNAPS
         do
                 if grep $SNAP ./backupSnaps/$SHORT_NAME >/dev/null
                 then
-                        EXISTING_SNAP=$SNAP
-                else
-                        if [ $EXISTING_SNAP == $NO_SNAP_NAME ]
-                        then
-                                echo $SNAP does not exist in backup, but neither does any parent snapshot, skipping
-                                continue
-                        fi
-                        if ! zfs send -Lecv -I $EXISTING_SNAP $LAST_SNAP > $TARGET_FILE
-                        then
-                                rm $TARGET_FILE
-                                echo FAILED: $DS from $EXISTING_SNAP to $LAST_SNAP
-                                exit 1
-                        fi
-                        break;
+                        REMOTE_SNAP=$SNAP
                 fi
         done
 
-        if [ $EXISTING_SNAP == $NO_SNAP_NAME ]
+        if [ $REMOTE_SNAP == $NO_SNAP_NAME ]
         then
-                echo Did not copy anything for $DS !
+                echo Could not find a starting snapshot for $DS !
+                exit 1
+        fi
+
+        if [ $REMOTE_SNAP == $SNAP_TO_SEND ]
+        then
+                echo Remote already has most recent backup for $DS
+                exit 1
+        fi
+
+        if ! zfs send -Lecv -I $REMOTE_SNAP $SNAP_TO_SEND > $TARGET_FILE
+        then
+                rm $TARGET_FILE
+                echo FAILED: $DS from $REMOTE_SNAP to $SNAP_TO_SEND
                 exit 1
         fi
 done
