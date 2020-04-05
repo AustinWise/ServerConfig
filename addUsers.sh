ae0a7dce6e8ff93a1f82b850339d234c121c1317
Austin Wise <AustinWise@gmail.com>
2020-04-04 17:48:42 -0700

    Map home directory when creating users.

--- addUsers.sh
+++ addUsers.sh
@@ -9,6 +9,5 @@ groupadd -g 1003 austin
 groupadd -g 1004 g
 groupadd wheel || true
 
-useradd -s /usr/bin/bash -g austin -G media,wheel -m -u 1001 austin
-useradd -s /usr/bin/bash -g g -G gmedia -m -u 1002 g
-
+useradd -s /usr/bin/bash -g austin -G media,wheel -m -u 1001  -d /zones/home/austin austin
+useradd -s /usr/bin/bash -g g -G gmedia -m -u 1002 -d /zones/home/g g
