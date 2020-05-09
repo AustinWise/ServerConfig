5ce3ed94530a34fe84bd2bf59571509a0cc03965
Austin Wise <AustinWise@gmail.com>
2020-05-08 22:58:20 -0700

    move folders

--- windowsBackup.sh
+++ windowsBackup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
-set -o pipefail
+#set -e
+#set -o pipefail
 set -x
 
 echo START > $HOME/backupLog.txt
@@ -12,14 +12,10 @@ SRC=/cygdrive
 
 rsync $RSYNC_OPT $SRC/d/src/ $DEST/src/
 rsync $RSYNC_OPT $SRC/d/AustinWise/ $DEST/AustinWise/
-rsync $RSYNC_OPT $SRC/c/Users/AustinWise/Downloads/ $DEST/DownAustin/
-rsync $RSYNC_OPT $SRC/c/Users/AustinWise/Desktop/ $DEST/DesktopAustin/
 rsync $RSYNC_OPT $SRC/c/Users/AustinWise/OneDrive/ $DEST/AustinOneDrive/
 rsync $RSYNC_OPT $SRC/c/Users/AustinWise/DropBox/ $DEST/AustinDropBox/
 rsync $RSYNC_OPT $SRC/c/Users/AustinWise/AppData/Roaming/Mozilla/Firefox/Profiles/kqn8ogux.default/ $DEST/FirefoxAustin/
 rsync $RSYNC_OPT $SRC/d/G/ $DEST/G/
-rsync $RSYNC_OPT $SRC/c/Users/G/Downloads/ $DEST/DownG/
-rsync $RSYNC_OPT $SRC/c/Users/G/Desktop/ $DEST/DesktopG/
 rsync $RSYNC_OPT $SRC/c/Users/G/AppData/Roaming/Mozilla/Firefox/Profiles/r82h6mbk.default/ $DEST/FirefoxG/
 
 echo done
